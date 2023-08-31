988ac33cf71da270177afbf570a9f8361303dea3
Jakob Reyes <jxreyes521@gmail.com>
2023-08-31 13:47:45 -0700

    Added Navbar, Cart, Shop, JS components

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div>
+            <h1>JakobDev Shop</h1>
+        </div>
+    </div>
+  )
+}
