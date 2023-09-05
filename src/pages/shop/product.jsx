8dc0a9fabd646ebf04082c0ba870ee40c14e62b5
Jakob Reyes <jxreyes521@gmail.com>
2023-09-05 13:39:19 -0700

    Finished product.jsx add to cart functionality

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shop-context';
 
 export const Product = (props) => {
     const {id, productName, price, productImg} = props.data;
+    const { addToCart, cartItems } = useContext(ShopContext);
+    const cartItemAmount = cartItems[id]
 
   return (
     <div className='product'>
         <img src={productImg} />
-        <div className="descritpion">
+        <div className="description">
             <p>
                 <b>{productName}</b>
             </p>
             <p>${price}</p>
         </div>
+        <button className='addToCartBtn' onClick={() => addToCart(id)}>
+            Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+        </button>
     </div>
   )
 }
